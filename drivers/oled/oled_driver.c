e35ec6728e66d41e5429d8c5c2ce1c0bef4ebf6d
Melissa Autumn <git@melissaautumn.com>
2025-08-31 21:03:20 -0700

    Adjust some always on bugs

--- drivers/oled/oled_driver.c
+++ drivers/oled/oled_driver.c
@@ -762,6 +762,7 @@ bool oled_on(void) {
         oled_active = true;
     }
 #ifdef OLED_ALWAYS_ON
+    oled_clear();
     oled_sleeping = false;
 #endif
     return oled_active;
@@ -771,7 +772,6 @@ bool oled_off(void) {
     if (!oled_initialized) {
         return !oled_active;
     }
-
 
 #ifdef OLED_ALWAYS_ON
     if (!oled_sleeping) {
@@ -981,7 +981,7 @@ void oled_task(void) {
 
     // Display timeout check
 #if OLED_TIMEOUT > 0
-    if (oled_active && timer_expired32(timer_read32(), oled_timeout)) {
+    if (is_oled_on() && timer_expired32(timer_read32(), oled_timeout)) {
         oled_off();
     }
 #endif
